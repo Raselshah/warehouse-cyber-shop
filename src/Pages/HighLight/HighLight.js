29287611ab0b12d93bdd38c3df3437ee54da7350
Md Rasel Shah <mdraselshah56@gmail.com>
2022-05-01 12:37:50 +0600

    extra hightlight  page add

---
+++ src/Pages/HighLight/HighLight.js
@@ -0,0 +1,127 @@
+import React from "react";
+
+const HighLight = () => {
+  return (
+    <div className="highlight-products grid grid-cols-3 w-5/6 mx-auto gap-8 mt-12">
+      <div className="electronic-products bg-white p-2">
+        <div className="product-title flex justify-between items-center p-2">
+          <h5>ELECTRONIC</h5>
+          <p className="text-xs text-gray-400">VIEW ALL CATEGORIES</p>
+        </div>
+        <div className="products-cards grid grid-cols-2 p-2 justify-center gap-4">
+          <div className="product-card">
+            <img
+              src="http://images.vinovathemes.com/prestashop_warehouse/html-image-cat1.jpg"
+              alt=""
+            />
+            <h4>Air Conditioner</h4>
+          </div>
+
+          <div className="product-card">
+            <img
+              src="http://images.vinovathemes.com/prestashop_warehouse/html-image-cat1.jpg"
+              alt=""
+            />
+            <h4>Air Conditioner</h4>
+          </div>
+
+          <div className="product-card">
+            <img
+              src="http://images.vinovathemes.com/prestashop_warehouse/html-image-cat1.jpg"
+              alt=""
+            />
+            <h4>Air Conditioner</h4>
+          </div>
+
+          <div className="product-card">
+            <img
+              src="http://images.vinovathemes.com/prestashop_warehouse/html-image-cat1.jpg"
+              alt=""
+            />
+            <h4>Air Conditioner</h4>
+          </div>
+        </div>
+      </div>
+      <div className="computer-products bg-white p-2">
+        <div className="product-title flex justify-between items-center p-2">
+          <h5>COMPUTER</h5>
+          <p className="text-xs text-gray-400">VIEW ALL CATEGORIES</p>
+        </div>
+        <div className="products-cards grid grid-cols-2 p-2 justify-center gap-4">
+          <div className="product-card">
+            <img
+              src="http://images.vinovathemes.com/prestashop_warehouse/html-image-cat1.jpg"
+              alt=""
+            />
+            <h4>Air Conditioner</h4>
+          </div>
+
+          <div className="product-card">
+            <img
+              src="http://images.vinovathemes.com/prestashop_warehouse/html-image-cat1.jpg"
+              alt=""
+            />
+            <h4>Air Conditioner</h4>
+          </div>
+
+          <div className="product-card">
+            <img
+              src="http://images.vinovathemes.com/prestashop_warehouse/html-image-cat1.jpg"
+              alt=""
+            />
+            <h4>Air Conditioner</h4>
+          </div>
+
+          <div className="product-card">
+            <img
+              src="http://images.vinovathemes.com/prestashop_warehouse/html-image-cat1.jpg"
+              alt=""
+            />
+            <h4>Air Conditioner</h4>
+          </div>
+        </div>
+      </div>
+      <div className="audio-products bg-white p-2">
+        <div className="product-title flex justify-between items-center p-2">
+          <h5>AUDIO</h5>
+          <p className="text-xs text-gray-400">VIEW ALL CATEGORIES</p>
+        </div>
+        <div className="products-cards grid grid-cols-2 p-2 justify-center gap-4">
+          <div className="product-card">
+            <img
+              src="http://images.vinovathemes.com/prestashop_warehouse/html-image-cat1.jpg"
+              alt=""
+            />
+            <h4>Air Conditioner</h4>
+          </div>
+
+          <div className="product-card">
+            <img
+              src="http://images.vinovathemes.com/prestashop_warehouse/html-image-cat1.jpg"
+              alt=""
+            />
+            <h4>Air Conditioner</h4>
+          </div>
+
+          <div className="product-card">
+            <img
+              src="http://images.vinovathemes.com/prestashop_warehouse/html-image-cat1.jpg"
+              alt=""
+            />
+            <h4>Air Conditioner</h4>
+          </div>
+
+          <div className="product-card">
+            <img
+              src="http://images.vinovathemes.com/prestashop_warehouse/html-image-cat1.jpg"
+              alt=""
+            />
+            <h4>Air Conditioner</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HighLight;
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,21 +1,12 @@
 import React from "react";
 import Banner from "../Banner/Banner";
+import HighLight from "../HighLight/HighLight";
 
 const HomePage = () => {
   return (
     <div>
       <Banner />
-      <div className="highlight-products grid grid-cols-3 w-4/5 mx-auto">
-        <div className="electronic-products">
-          <h2>gg</h2>
-        </div>
-        <div className="computer-products">
-          <h2>gah</h2>
-        </div>
-        <div className="audio-products">
-          <h2>dsfg</h2>
-        </div>
-      </div>
+      <HighLight />
     </div>
   );
 };
