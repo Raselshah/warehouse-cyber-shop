5334691e1d75a51d0078e0e495d60e7c4d83a5d8
Md Rasel Shah <mdraselshah56@gmail.com>
2022-05-05 12:11:47 +0600

    use react form and data send to api

--- src/Pages/AddNewItems/AddNewItems.js
+++ src/Pages/AddNewItems/AddNewItems.js
@@ -1,21 +1,69 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const AddNewItems = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+    const url = `http://localhost:5000/home`;
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((product) => {
+        console.log(product);
+      });
+  };
   return (
     <div className="text-center mt-12">
       <h2>Please enter your new products</h2>
-      <form className="flex flex-col w-4/6 mx-auto gap-4 p-2 mt-12">
-        <input type="text" name="sname" id="" placeholder="Supplier name" />
-        <input type="text" name="pname" id="" placeholder="product name" />
-        <textarea
-          type="text"
-          name="description"
-          id=""
-          placeholder="product info.."
+      <form className="mt-4" onSubmit={handleSubmit(onSubmit)}>
+        <input
+          className="flex flex-col mb-2 p-2 lg:w-1/3 mx-auto border-b-2 md:w-5/6"
+          placeholder="Supplier Name"
+          {...register("supplierName", { required: true, maxLength: 20 })}
         />
-        <input type="number" name="price" id="" placeholder="price" />
-
-        <button>Save</button>
+        <input
+          className="flex flex-col mb-2 p-2 lg:w-1/3 mx-auto border-b-2 md:w-5/6"
+          placeholder="Product Name"
+          {...register("productName")}
+        />
+        <textarea
+          className="flex flex-col mb-2 p-2 lg:w-1/3 mx-auto border-b-2 md:w-5/6"
+          placeholder="description"
+          {...register("description")}
+        />
+        <input
+          className="flex flex-col mb-2 p-2 lg:w-1/3 mx-auto border-b-2 md:w-5/6"
+          placeholder="category"
+          {...register("category")}
+        />
+        <input
+          className="flex flex-col mb-2 p-2 lg:w-1/3 mx-auto border-b-2 md:w-5/6"
+          placeholder="photo url"
+          {...register("picture")}
+        />
+        <input
+          className="flex flex-col mb-2 p-2 lg:w-1/3 mx-auto border-b-2 md:w-5/6"
+          placeholder="price"
+          type="number"
+          {...register("price")}
+        />
+        <input
+          className="flex flex-col mb-2 p-2 lg:w-1/3 mx-auto border-b-2 md:w-5/6"
+          placeholder="quantity"
+          type="number"
+          {...register("quantity")}
+        />
+        <input
+          className="mt-2 bg-sky-600 hover:bg-sky-700 hover:text-white w-40 py-2 cursor-pointer rounded"
+          value="SAVE DATA"
+          type="submit"
+        />
       </form>
     </div>
   );
