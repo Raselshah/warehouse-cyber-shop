67ec81957e3962ad72c5c9d1c6666474e5bf5a3d
Md Rasel Shah <mdraselshah56@gmail.com>
2022-05-08 13:16:16 +0600

    ui and update button create

--- src/Pages/InventoryItem/InventoryItem.js
+++ src/Pages/InventoryItem/InventoryItem.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { TiDeleteOutline } from "react-icons/ti";
+import { MdUpdate } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
 const InventoryItem = ({ product, handleProductDelete }) => {
   const { _id, price, picture, supplierName, description, quantity, category } =
     product;
+
+  const navigate = useNavigate();
 
   return (
     <>
@@ -16,7 +20,13 @@ const InventoryItem = ({ product, handleProductDelete }) => {
         <td className="px-4 text-gray-600">${price}</td>
         <td className="px-4 text-gray-600">{supplierName}</td>
         <td className="px-4">{quantity}</td>
-        <td className="px-4">
+        <td className="px-4 flex">
+          <button onClick={() => navigate(`/inventory/${_id}`)}>
+            <p title="update" className="mx-2 text-3xl">
+              <MdUpdate />
+            </p>
+          </button>
+
           <button onClick={() => handleProductDelete(_id)}>
             <p title="Delete" className=" text-3xl">
               <TiDeleteOutline />
